8bd586e41e8a69da8fe77c20d902224fd3b8f463
Bryn Bennett <bryn.bennett@gmail.com>
2019-03-21 05:44:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,16 +11,15 @@ class Student
   end 
   
   def self.create_table
-    sql = <<-SQL 
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT,
-      )
-    SQL
-    
-    DB[:conn].execute(sql)
-  end 
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
   
   def self.drop_table 
     sql = <<-SQL 
